d6a4f76d09d03a7b0ed6e7edfb6c564cd511de32
Nathan Griffiths <ngriffiths@postmedia.com>
2024-10-06 17:58:48 -0700

    Updates

---
+++ precache-manifest.adc4a379465c62f1402c73be1c2af823.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f26b22f448879eb92a94a9b31b8550",
+    "url": "/elxn24-campaign-stops/index.html"
+  },
+  {
+    "revision": "0e4a2076313bbbd00de1",
+    "url": "/elxn24-campaign-stops/static/css/main.0bd3b5cf.chunk.css"
+  },
+  {
+    "revision": "48070cc15c28b9eb18d2",
+    "url": "/elxn24-campaign-stops/static/js/2.825496b8.chunk.js"
+  },
+  {
+    "revision": "e229a5041e674faa61916b41dd6f3b44",
+    "url": "/elxn24-campaign-stops/static/js/2.825496b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4a2076313bbbd00de1",
+    "url": "/elxn24-campaign-stops/static/js/main.be4eb183.chunk.js"
+  },
+  {
+    "revision": "5d80c3ec7dfe9e587f69",
+    "url": "/elxn24-campaign-stops/static/js/runtime-main.a3509959.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/elxn24-campaign-stops/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/elxn24-campaign-stops/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/elxn24-campaign-stops/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/elxn24-campaign-stops/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/elxn24-campaign-stops/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/elxn24-campaign-stops/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
